4fbf1a111769f456a121e8d502f00f4dc99f293d
Rafael Jaimes III <rafjaimes@gmail.com>
2019-05-08 11:24:20 -0400

    Update rhythm.m

--- rhythm.m
+++ rhythm.m
@@ -1,7 +1,6 @@
 function rhythm
 % close all; clc;
 %% RHYTHM (01/27/2012)
-%test on github
 % Matlab software for analyzing optical mapping data
 %
 % By Matt Sulkin, Jake Laughner, Xinyuan Sophia Cui, Jed Jackoway
